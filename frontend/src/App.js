76232e321705881d9323b351affe041d36f575ca
dcarroll9398 <dcarroll9398@yahoo.com>
2022-03-25 16:12:15 -0500

    Video-05-Show-List-Of-Products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,32 @@
+import data from './data';
 
 function App() {
   return (
     <div>
-    <header>
-     <a href="/">amazona</a>
-    </header>
-    <main>
-      <h1>list posts</h1>
-    </main>
+      <header>
+        <a href="/">amazona</a>
+      </header>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.name}`}>
+                <img src={product.image} alt={product.slug} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
